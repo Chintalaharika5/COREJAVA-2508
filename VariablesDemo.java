be501ea95b992d962710cdcc76a1c81a56816f09
Chintalaharika5 <chintalaharika5@gmail.com>
2025-06-16 09:13:03 +0530

    update

---
+++ VariablesDemo.java
@@ -0,0 +1,8 @@
+public class VariablesDemo {
+    public static void main(String[] args){
+        int age = 20;
+        String name = "Ravi";
+        String message = "Welcome To Java Programming";
+        System.out.println(message);
+    }
+}
