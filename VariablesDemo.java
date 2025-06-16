3e8e88dc32e6c02fb8a7afbd9f25266e9c343ffa
Chintalaharika5 <chintalaharika5@gmail.com>
2025-06-16 09:29:34 +0530

    UPDATE

--- VariablesDemo.java
+++ VariablesDemo.java
@@ -1,8 +1,25 @@
 public class VariablesDemo {
     public static void main(String[] args){
         int age = 20;
-        String name = "Ravi";
+        byte bytenumber = 100;
+        short shortnumber = 1000;
+        int intNumber = 20;
+        int longNumber = 20000;
+        float floatNumber = 3.5f;
+        double doubleNumber = 3.14;
+        boolean booleanValue = true;
+        char charValue = 'H';
+
+        String name = "Harika";
         String message = "Welcome To Java Programming";
         System.out.println(message);
+        System.out.println(bytenumber);
+        System.out.println(shortnumber);
+        System.out.println(intNumber);
+        System.out.println(longNumber);
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+        System.out.println(booleanValue);
+        System.out.println(charValue);
     }
 }
